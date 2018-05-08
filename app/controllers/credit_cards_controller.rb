f6ead08cf4b4e9cbb0514c71c79e01c4fcbd5542
Juan M. Panasiti <juanpanasiti@gmail.com>
2018-05-08 19:34:42 -0300

    Agregada la opción de editar TCs ya creadas

--- app/controllers/credit_cards_controller.rb
+++ app/controllers/credit_cards_controller.rb
@@ -1,4 +1,6 @@
 class CreditCardsController < ApplicationController
+  before_action :set_credit_card, only: [:edit, :update]
+
   def new
     @credit_card = CreditCard.new
   end
@@ -10,8 +12,24 @@ class CreditCardsController < ApplicationController
     end
   end
 
+  def edit
+    #code
+  end
+  def update
+    respond_to do |format|
+      if @credit_card.update(credit_card_params)
+        format.html { redirect_to root_url, notice: 'Tarjeta de Crédito modificada.' }
+      else
+        format.html { redirect_to root_url, alert: 'Error!!' }
+      end
+    end
+  end
+
   protected
   def credit_card_params
     params.require(:credit_card).permit(:user_id, :issuer, :kind, :expiration)
+  end
+  def set_credit_card
+    @credit_card = CreditCard.find(params[:id])
   end
 end
